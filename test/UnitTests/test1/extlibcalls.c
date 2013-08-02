e7ff24d2bb30a4f4f3a8c55a4c8bd75e23eb9a73
Mingliang LIU <liuml07@gmail.com>
2013-08-02 17:39:55 +0800

    Simplify the test1
    It seems that the abnormal thing lies in the strlen() function call.

--- test/UnitTests/test1/extlibcalls.c
+++ test/UnitTests/test1/extlibcalls.c
@@ -2,30 +2,18 @@
 #include <string.h>
 #include <stdlib.h>
 
+#define LENGTH 128
+
 int main(int argc, char *argv[])
 {
-  char *input1=NULL, *input2=NULL, *input3=NULL;
-  int length1, length2, length3;
-
-  length1 = 100;
-  length2 = 50;
-  length3 = 150;
+  char *input = (char *)malloc(LENGTH);
+  memset(input, 0, LENGTH);
   
-  input1 = (char *) malloc(length1);
-  input2 = (char *) calloc(length2, 1+argc);
-  input3 = (char *) calloc(2, length1);
-  memset(input3, 0, 2*length1);
-  //memset(input1, 0, 2*length1);
+  strcpy(input, argv[1]);
+  strcat(input, argv[2]);
 
-  strcpy(input1, argv[1]);
-  strcat(input1, argv[2]);
-  memcpy(input2, input1, 11);
-  memmove(input3, input2, 1);
-  sprintf(input3, "%s %s %s\n", input1, input2, input3);
+  printf("%s\n", input);
+  printf("%zd\n", strlen(input));
 
-  printf("%s %s\n", input1, input2);
-  //return strlen(input2);
-  //scanf("%s", input1);
-
-  return strlen(input3);
+  return strlen(input);
 }
