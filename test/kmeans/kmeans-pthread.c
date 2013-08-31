abb9bf5f0821522f461f491a2ed93d88624fbddd
Mingliang LIU <liuml07@gmail.com>
2013-08-31 16:45:27 +0800

    Add criterion files from kmeans test

--- test/kmeans/kmeans-pthread.c
+++ test/kmeans/kmeans-pthread.c
@@ -38,7 +38,7 @@
 #include "stddefines.h"
 #include "map_reduce.h"
 
-#define DEF_NUM_POINTS 1000
+#define DEF_NUM_POINTS 100
 #define DEF_NUM_MEANS 10
 #define DEF_DIM 3
 #define DEF_GRID_SIZE 10
@@ -358,5 +358,5 @@ int main(int argc, char **argv)
    free(means);
    free(clusters);
 
-   return means[0][0];
+   return 0;
 }
